2ffe7b017f91e48726a63480f4c391f7d57b6e22
Jendoliver <alvarezmonllor@gmail.com>
2017-09-29 20:33:58 +0200

    Some loko improvements on HelloWorld

--- FirstASApp/app/src/main/java/com/apporelbotna/firstasapp/MainActivity.java
+++ FirstASApp/app/src/main/java/com/apporelbotna/firstasapp/MainActivity.java
@@ -2,15 +2,37 @@ package com.apporelbotna.firstasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.tv_olga);
-        textView.setText(getResources().getIdentifier("converted_main", "string", "values"));
+        textView = (TextView) findViewById(R.id.tv_olga);
+
+        // Events
+        Button syy = (Button) findViewById(R.id.btn1);
+        syy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Ese syy ke syyyyy");
+            }
+        });
+
+        Button noo = (Button) findViewById(R.id.btn2);
+        final EditText textInput = (EditText) findViewById(R.id.input1);
+        noo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(textInput.getText());
+            }
+        });
     }
 }
