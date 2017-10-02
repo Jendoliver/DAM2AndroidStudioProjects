60751e4408a72a933723af3ff5ffd0e6fc4923c8
Jendoliver <alvarezmonllor@gmail.com>
2017-10-02 18:02:09 +0200

    Background color buttons

--- FirstASApp/app/src/main/java/com/apporelbotna/firstasapp/MainActivity.java
+++ FirstASApp/app/src/main/java/com/apporelbotna/firstasapp/MainActivity.java
@@ -1,28 +1,32 @@
 package com.apporelbotna.firstasapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    RelativeLayout mainBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.tv_olga);
+        mainBackground = (RelativeLayout) findViewById(R.id.mainBackground);
 
         // Events
         Button syy = (Button) findViewById(R.id.btn1);
         syy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("Ese syy ke syyyyy");
+                textView.setText("Ese syy ke patsdonidas");
             }
         });
 
@@ -32,6 +36,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textView.setText(textInput.getText());
+            }
+        });
+
+        Button btnRed = (Button) findViewById(R.id.btnRed);
+        btnRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainBackground.setBackgroundColor(Color.RED);
+            }
+        });
+
+        Button btnGreen = (Button) findViewById(R.id.btnGreen);
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainBackground.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+        Button btnBlue = (Button) findViewById(R.id.btnBlue);
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainBackground.setBackgroundColor(Color.BLUE);
             }
         });
     }
